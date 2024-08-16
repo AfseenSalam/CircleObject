b46cf45a6901d0a00890d5c0d49b81d9e6a6721d
Afseen Salam <afseenfazil@gmail.com>
2024-08-16 16:58:08 -0400

    initial commit

---
+++ CircleObject/Program.cs
@@ -0,0 +1,59 @@
+﻿using CircleObject;
+using System.ComponentModel.DataAnnotations;
+using System.Net.Http.Headers;
+using System.Reflection.Metadata.Ecma335;
+
+
+Console.WriteLine("==================");
+Console.WriteLine("CIRCLE OBJECT");
+Console.WriteLine("==================");
+Console.WriteLine("Enter the radius:");
+double radius;
+string? input = Console.ReadLine();
+bool Isvalid = Validatation.inputValidating(input, out radius);
+while (!Isvalid)
+{
+    Console.WriteLine("Invalid input please enter again:");
+    Isvalid = Validatation.inputValidating(Console.ReadLine(), out radius);
+}
+Circle myCircle = new Circle(radius);
+Console.WriteLine($"Radius:{myCircle.GetRadius()}");
+Console.WriteLine($"Diameter:{myCircle.CalculateDiameter()}");
+Console.WriteLine($"Circumference:{myCircle.CalculateCircumference()}");
+Console.WriteLine($"Area:{myCircle.CalculateArea()}");
+Console.WriteLine("");
+
+while (Isvalid)
+{
+    Console.WriteLine("Should the circle grow? (y/n):");
+    string? userChoice = Console.ReadLine();
+    if (userChoice.ToLower() == "y")
+    {
+        myCircle.Grow();
+        Console.WriteLine($"NewDiameter:{myCircle.CalculateDiameter()}");
+        Console.WriteLine($"NewCircumference:{myCircle.CalculateCircumference()}");
+        Console.WriteLine($"NewArea:{myCircle.CalculateArea()}");
+        Isvalid = true;
+    }
+    else if(userChoice.ToLower() == "n")
+    {
+        Console.WriteLine($"Goodbye. The circle’s final radius is {myCircle.GetRadius()}");
+        Isvalid = false;
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Invalid input:");
+        Isvalid = true;
+    }
+}
+
+   
+    
+
+
+
+
+
+
+  
---
+++ CircleObject/Validatation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleObject
+{
+    internal class Validatation
+    {
+        public static bool inputValidating(string i, out double rad)
+        {
+            if (double.TryParse(i, out rad) && rad > 0)
+            {
+                return true;
+            }
+            return false;
+
+        }
+        
+    }
+}
